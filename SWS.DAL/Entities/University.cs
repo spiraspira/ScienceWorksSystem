efc9ee35e4d5bb1b535dcbe79c4bf9a8d113ee69
spiraspira <zabornik5350@gmail.com>
2024-04-20 18:31:06 +0300

    Add University entity

---
+++ SWS.DAL/Entities/University.cs
@@ -0,0 +1,8 @@
+﻿namespace SWS.DAL.Entities;
+
+public class University : Entity
+{
+	public string Name { get; set; }
+
+	public string Location { get; set; }
+}
--- SWS.DAL/Entities/User.cs
+++ SWS.DAL/Entities/User.cs
@@ -2,9 +2,13 @@
 
 public class User : Entity
 {
-	public string Login { get; set; }
+	public string? Login { get; set; }
 
-	public string Password { get; set; }
+	public string? Password { get; set; }
 
-	public string Name { get; set; }
+	public string? Name { get; set; }
+
+	public Guid? UniversityId { get; set; }
+
+	public University? University { get; set; }
 }
