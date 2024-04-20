c24d383d749b7d4896ec6d794588f8382ac43286
spiraspira <zabornik5350@gmail.com>
2024-04-20 19:45:28 +0300

    Add AppDbContext

---
+++ SWS.DAL/Entities/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿namespace SWS.DAL.Entities;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+	public DbSet<Committee>? Committees { get; set; }
+
+	public DbSet<CommitteeMember>? CommitteeMembers { get; set; }
+
+	public DbSet<Contest>? Contests { get; set; }
+
+	public DbSet<Grade>? Grades { get; set; }
+
+	public DbSet<Report>? Reports { get; set; }
+
+	public DbSet<Review>? Reviews { get; set; }
+
+	public DbSet<Student>? Students { get; set; }
+
+	public DbSet<Teacher>? Teachers { get; set; }
+
+	public DbSet<Team>? Teams { get; set; }
+
+	public DbSet<University>? Universities { get; set; }
+
+	public DbSet<User>? Users { get; set; }
+}
