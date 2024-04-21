f729bc571b5b4eba0de3519cc4acb615f0abde61
spiraspira <zabornik5350@gmail.com>
2024-04-21 15:27:03 +0300

    Add Automapper

---
+++ SWS.BLL/Mapper/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿namespace SWS.BLL.Mapper;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<CommitteeMember, CommitteeMemberModel>().ReverseMap();
+
+		CreateMap<Committee, CommitteeModel>().ReverseMap();
+
+		CreateMap<Contest, ContestModel>().ReverseMap();
+
+		CreateMap<Grade, GradeModel>().ReverseMap();
+
+		CreateMap<Report, ReportModel>().ReverseMap();
+
+		CreateMap<Review, ReviewModel>().ReverseMap();
+
+		CreateMap<Student, StudentModel>().ReverseMap();
+
+		CreateMap<Teacher, TeacherModel>().ReverseMap();
+
+		CreateMap<Team, TeamModel>().ReverseMap();
+
+		CreateMap<University, UniversityModel>().ReverseMap();
+
+		CreateMap<User, UserModel>().ReverseMap();
+	}
+}
