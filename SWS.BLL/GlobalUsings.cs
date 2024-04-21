bd9bba74c3a9946315c152b18d8b3732d57c6c60
spiraspira <zabornik5350@gmail.com>
2024-04-21 15:09:35 +0300

    Add IModel

---
+++ SWS.BLL/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// Global using directives
+
+global using Microsoft.EntityFrameworkCore.Metadata;
+global using IModel = SWS.BLL.Interfaces.IModel;
---
+++ SWS.BLL/Models/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace SWS.BLL.Models;
+
+public class Entity : IModel
+{
+	public Guid Id { get; set; }
+}
