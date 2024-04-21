5b6cbf51c8d22927b273069526b917381d35f4f9
spiraspira <zabornik5350@gmail.com>
2024-04-21 14:58:23 +0300

    Add DataAccessRegister

---
+++ SWS.DAL/DI/DataAccessRegister.cs
@@ -0,0 +1,37 @@
+﻿#pragma warning disable S125
+namespace SWS.DAL.DI;
+
+public static class DataAccessRegister
+{
+	public static void AddDataAccess(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			options.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+		});
+
+		//services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+		services.AddScoped<ICommitteeMemberRepository, CommitteeMemberRepository>();
+
+		services.AddScoped<ICommitteeRepository, CommitteeRepository>();
+
+		services.AddScoped<IContestRepository, ContestRepository>();
+
+		services.AddScoped<IGradeRepository, GradeRepository>();
+
+		services.AddScoped<IReportRepository, ReportRepository>();
+
+		services.AddScoped<IReviewRepository, ReviewRepository>();
+
+		services.AddScoped<IStudentRepository, StudentRepository>();
+
+		services.AddScoped<ITeacherRepository, TeacherRepository>();
+
+		services.AddScoped<ITeamRepository, TeamRepository>();
+
+		services.AddScoped<IUniversityRepository, UniversityRepository>();
+
+		services.AddScoped<IUserRepository, UserRepository>();
+	}
+}
--- SWS.DAL/GlobalUsings.cs
+++ SWS.DAL/GlobalUsings.cs
@@ -1,5 +1,8 @@
 // Global using directives
 
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using SWS.DAL.Entities;
 global using SWS.DAL.Interfaces;
+global using SWS.DAL.Repositories;
