47220b16ebf419b876108e381d0b5eb10f1c83a5
spiraspira <zabornik5350@gmail.com>
2024-04-21 07:51:47 +0300

    Add CommitteeRepository

---
+++ SWS.DAL/Repositories/CommitteeRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace SWS.DAL.Repositories;
+
+public class CommitteeRepository(ApplicationDbContext context) : GenericRepository<Committee>(context)
+{
+	public override Task<Committee?> Get(Guid id)
+	{
+		return Set
+			.Include(committee => committee.Teacher)
+			.FirstOrDefaultAsync(p => p.Id == id);
+	}
+
+	public override async Task<IEnumerable<Committee>> GetAll()
+	{
+		return await Set
+			.Include(committee => committee.Teacher)
+			.ToListAsync();
+	}
+}
