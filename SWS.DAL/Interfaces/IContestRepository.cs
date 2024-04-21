9c5c3a7eb1abbf7815d02c8fa55cc294a8017ba8
spiraspira <zabornik5350@gmail.com>
2024-04-21 08:25:26 +0300

    Add ContestRepository

---
+++ SWS.DAL/Interfaces/IContestRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SWS.DAL.Interfaces;
+
+public interface IContestRepository : IGenericRepository<Contest>
+{
+	Task<IEnumerable<Contest>> GetActiveContests();
+
+	Task<IEnumerable<Contest>> GetFinishedContests();
+
+	Task<IEnumerable<Contest>> GetActiveContestsOfTeacherAsInvited(Guid teacherId);
+
+	Task<IEnumerable<Contest>> GetFinishedContestsOfTeacherAsInvited(Guid teacherId);
+}
---
+++ SWS.DAL/Repositories/ContestRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace SWS.DAL.Repositories;
+
+public class ContestRepository(ApplicationDbContext context) : GenericRepository<Contest>(context), IContestRepository
+{
+	public override Task<Contest?> Get(Guid id)
+	{
+		return Set
+			.Include(contest => contest.InvitedTeacher)
+			.ThenInclude(teacher => teacher!.User)
+			.FirstOrDefaultAsync(p => p.Id == id);
+	}
+
+	public override async Task<IEnumerable<Contest>> GetAll()
+	{
+		return await Set
+			.Include(contest => contest.InvitedTeacher)
+			.ThenInclude(teacher => teacher!.User)
+			.ToListAsync();
+	}
+
+	public async Task<IEnumerable<Contest>> GetActiveContests()
+	{
+		return await Set
+			.Include(contest => contest.InvitedTeacher)
+			.ThenInclude(teacher => teacher!.User)
+			.Where(contest => contest.DateEnd >= DateTime.Now)
+			.ToListAsync();
+	}
+
+	public async Task<IEnumerable<Contest>> GetFinishedContests()
+	{
+		return await Set
+			.Include(contest => contest.InvitedTeacher)
+			.ThenInclude(teacher => teacher!.User)
+			.Where(contest => contest.DateEnd < DateTime.Now)
+			.ToListAsync();
+	}
+
+	public async Task<IEnumerable<Contest>> GetActiveContestsOfTeacherAsInvited(Guid teacherId)
+	{
+		return await Set
+			.Include(contest => contest.InvitedTeacher)
+			.ThenInclude(teacher => teacher!.User)
+			.Where(contest => contest.DateEnd >= DateTime.Now && contest.InvitedTeacherId == teacherId)
+			.ToListAsync();
+	}
+
+	public async Task<IEnumerable<Contest>> GetFinishedContestsOfTeacherAsInvited(Guid teacherId)
+	{
+		return await Set
+			.Include(contest => contest.InvitedTeacher)
+			.ThenInclude(teacher => teacher!.User)
+			.Where(contest => contest.DateEnd < DateTime.Now && contest.InvitedTeacherId == teacherId)
+			.ToListAsync();
+	}
+}
