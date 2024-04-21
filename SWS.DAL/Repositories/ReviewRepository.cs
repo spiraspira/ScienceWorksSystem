fab17515c61b10639b5a2238b71e891b37918aa0
spiraspira <zabornik5350@gmail.com>
2024-04-21 14:50:09 +0300

    Add ReviewRepository

---
+++ SWS.DAL/Repositories/ReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SWS.DAL.Repositories;
+
+public class ReviewRepository(ApplicationDbContext context) : GenericRepository<Review>(context), IReviewRepository
+{
+	public async Task<IEnumerable<Review>> GetReviewsOfReport(Guid reportId)
+	{
+		return await Set
+			.Include(review => review.OrganizationCommitteeMember)
+			.Where(review => review.ReportId == reportId)
+			.ToListAsync();
+	}
+}
