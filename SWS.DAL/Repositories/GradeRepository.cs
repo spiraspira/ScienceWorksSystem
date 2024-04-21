425467ae3837fd7d8f2c4ce2d822deb7536def29
spiraspira <zabornik5350@gmail.com>
2024-04-21 14:48:06 +0300

    Add GradeRepository

---
+++ SWS.DAL/Repositories/GradeRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace SWS.DAL.Repositories;
+
+public class GradeRepository(ApplicationDbContext context) : GenericRepository<Grade>(context), IGradeRepository
+{
+	public async Task<IEnumerable<Grade>> GetGradesOfReport(Guid reportId)
+	{
+		return await Set
+			.Include(grade => grade.ProgramCommitteeMember)
+			.Where(grade => grade.ReportId == reportId)
+			.ToListAsync();
+	}
+}
