0d9b30515e2ccc184619192ddece82f4423a6447
spiraspira <zabornik5350@gmail.com>
2024-04-29 02:18:48 +0300

    Add UserService

---
+++ SWS.BLL/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS9107 // Parameter is captured into the state of the enclosing type and its value is also passed to the base constructor. The value might be captured by the base class as well.
+namespace SWS.BLL.Services;
+
+public class UserService(IUserRepository repository, IMapperBase mapper) : GenericService<UserModel, User>(repository, mapper), IUserService
+{
+	public async Task<UserModel> Login(string login, string password)
+	{
+		var user = await repository.Login(login, password);
+
+		var userModel = mapper.Map<UserModel>(user);
+
+		return userModel;
+	}
+}
