73e65cea8405d99d9898b8b109f8fbc1ed4d819f
spiraspira <zabornik5350@gmail.com>
2024-04-29 04:25:52 +0300

    Add ReviewService

---
+++ SWS.BLL/Services/ReviewService.cs
@@ -0,0 +1,11 @@
+﻿namespace SWS.BLL.Services;
+
+public class ReviewService(IReviewRepository repository, IMapperBase mapper) : GenericService<ReviewModel, Review>(repository, mapper), IReviewService
+{
+	public async Task<IEnumerable<ReviewModel>> GetReviewsOfReport(Guid reportId)
+	{
+		var review = await repository.GetReviewsOfReport(reportId);
+
+		return mapper.Map<IEnumerable<ReviewModel>>(review);
+	}
+}
