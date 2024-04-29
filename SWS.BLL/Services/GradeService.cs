74cb31431833e86b20ef0f7b2ea30bbf6ad355c7
spiraspira <zabornik5350@gmail.com>
2024-04-29 04:31:45 +0300

    Add GradeService

---
+++ SWS.BLL/Services/GradeService.cs
@@ -0,0 +1,11 @@
+﻿namespace SWS.BLL.Services;
+
+public class GradeService(IGradeRepository repository, IMapperBase mapper) : GenericService<GradeModel, Grade>(repository, mapper), IGradeService
+{
+	public async Task<IEnumerable<GradeModel>> GetGradesOfReport(Guid reportId)
+	{
+		var grades = await repository.GetGradesOfReport(reportId);
+
+		return mapper.Map<IEnumerable<GradeModel>>(grades);
+	}
+}
