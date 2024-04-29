3a2e1cddeaea17ffdfe18e3b50fcc34713468e32
spiraspira <zabornik5350@gmail.com>
2024-04-29 03:03:22 +0300

    Add NominationService

---
+++ SWS.BLL/Interfaces/INominationService.cs
@@ -0,0 +1,6 @@
+﻿namespace SWS.BLL.Interfaces;
+
+public interface INominationService : IGenericService<NominationModel>
+{
+	Task<IEnumerable<NominationModel>> GetNominationsOfContest(Guid contestId);
+}
