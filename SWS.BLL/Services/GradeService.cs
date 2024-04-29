2fcc054c054dcf99615f2d4fa5d4fdaeed451bd5
spiraspira <zabornik5350@gmail.com>
2024-04-29 05:21:53 +0300

    Disable excess warnings

--- SWS.BLL/Services/GradeService.cs
+++ SWS.BLL/Services/GradeService.cs
@@ -1,4 +1,5 @@
-﻿namespace SWS.BLL.Services;
+﻿#pragma warning disable CS9107 // Parameter is captured into the state of the enclosing type and its value is also passed to the base constructor. The value might be captured by the base class as well.
+namespace SWS.BLL.Services;
 
 public class GradeService(IGradeRepository repository, IMapperBase mapper) : GenericService<GradeModel, Grade>(repository, mapper), IGradeService
 {
