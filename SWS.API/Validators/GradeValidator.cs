434ae14c7df292680795ffe4b3d098e3cbf68ce9
spiraspira <zabornik5350@gmail.com>
2024-04-29 13:05:17 +0300

    Add GradeValidator

---
+++ SWS.API/Validators/GradeValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace SWS.API.Validators;
+
+public class GradeValidator : AbstractValidator<GradeViewModel>
+{
+	public GradeValidator()
+	{
+		RuleFor(grade => grade.Text)
+			.NotEmpty();
+
+		RuleFor(grade => grade.ReportGrade)
+			.NotNull()
+			.InclusiveBetween(1, 5);
+
+		RuleFor(grade => grade.ReportId)
+			.NotNull();
+
+		RuleFor(grade => grade.ProgramCommitteeMemberId)
+			.NotNull();
+
+		RuleFor(grade => grade.NominationId)
+			.NotNull();
+	}
+}
