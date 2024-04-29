d4bb0bae5327c628e68ee7426ccdcd1c259aa1a5
spiraspira <zabornik5350@gmail.com>
2024-04-29 05:59:03 +0300

    Add BusinessLogicRegister

---
+++ SWS.BLL/DI/BusinessLogicRegister.cs
@@ -0,0 +1,33 @@
+﻿namespace SWS.BLL.DI;
+
+public static class BusinessLogicRegister
+{
+	public static void AddBusinessLogic(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddScoped<ICommitteeMemberService, CommitteeMemberService>();
+
+		services.AddScoped<ICommitteeService, CommitteeService>();
+
+		services.AddScoped<IContestService, ContestService>();
+
+		services.AddScoped<IGradeService, GradeService>();
+
+		services.AddScoped<INominationService, NominationService>();
+
+		services.AddScoped<IReportService, ReportService>();
+
+		services.AddScoped<IReviewService, ReviewService>();
+
+		services.AddScoped<IStudentService, StudentService>();
+
+		services.AddScoped<ITeacherService, TeacherService>();
+
+		services.AddScoped<ITeamService, TeamService>();
+
+		services.AddScoped<IUniversityService, UniversityService>();
+
+		services.AddScoped<IUserService, UserService>();
+
+		services.AddDataAccess(config);
+	}
+}
--- SWS.BLL/GlobalUsings.cs
+++ SWS.BLL/GlobalUsings.cs
@@ -2,8 +2,12 @@
 
 global using AutoMapper;
 global using Microsoft.EntityFrameworkCore.Metadata;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using SWS.BLL.Interfaces;
 global using SWS.BLL.Models;
+global using SWS.BLL.Services;
+global using SWS.DAL.DI;
 global using SWS.DAL.Entities;
 global using SWS.DAL.Interfaces;
 global using IModel = SWS.BLL.Interfaces.IModel;
