5734c1a8c90dc6eeaa1ec0935fb7484d9a1a9922
spiraspira <zabornik5350@gmail.com>
2024-04-29 06:52:12 +0300

    Add CommitteeValidator and CommitteeMemberValidator

---
+++ SWS.API/Validators/CommitteeMemberValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace SWS.API.Validators;
+
+public class CommitteeMemberValidator : AbstractValidator<CommitteeMemberViewModel>
+{
+	public CommitteeMemberValidator()
+	{
+		RuleFor(committeeMember => committeeMember.CommitteeId)
+			.NotNull();
+
+		RuleFor(committeeMember => committeeMember.TeacherId)
+			.NotNull();
+	}
+}
---
+++ SWS.API/Validators/CommitteeValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace SWS.API.Validators;
+
+public class CommitteeValidator : AbstractValidator<CommitteeViewModel>
+{
+	public CommitteeValidator()
+	{
+		RuleFor(committee => committee.TeacherId)
+			.NotNull();
+	}
+}
