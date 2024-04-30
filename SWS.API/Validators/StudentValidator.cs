fb2273423f9dcc25d5392e65552d0d89fc5f7e94
spiraspira <zabornik5350@gmail.com>
2024-04-30 18:22:18 +0300

    Add Student and Teacher validators

---
+++ SWS.API/Validators/StudentValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace SWS.API.Validators;
+
+public class StudentValidator : AbstractValidator<StudentViewModel>
+{
+	public StudentValidator()
+	{
+		RuleFor(student => student.UserId)
+			.NotNull();
+	}
+}
