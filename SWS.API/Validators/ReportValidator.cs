eb44c5f3f42ade6031b875db47b6619aa11d45e8
spiraspira <zabornik5350@gmail.com>
2024-04-30 18:18:27 +0300

    Add ReportValidator

---
+++ SWS.API/Validators/ReportValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace SWS.API.Validators;
+
+public class ReportValidator : AbstractValidator<ReportViewModel>
+{
+	public ReportValidator()
+	{
+		RuleFor(report => report.Name)
+			.NotEmpty();
+
+		RuleFor(report => report.File)
+			.NotEmpty();
+
+		RuleFor(report => report.TeamId)
+			.NotNull();
+
+		RuleFor(report => report.ContestId)
+			.NotNull();
+	}
+}
