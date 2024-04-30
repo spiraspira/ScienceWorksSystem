9b167e3adbf6bd9aac6ef93d5a40f54761073928
spiraspira <zabornik5350@gmail.com>
2024-04-30 18:27:38 +0300

    Add UserValidator

---
+++ SWS.API/Validators/UserValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace SWS.API.Validators;
+
+public class UserValidator : AbstractValidator<UserViewModel>
+{
+	public UserValidator()
+	{
+		RuleFor(user => user.Login)
+			.NotEmpty();
+
+		RuleFor(user => user.Password)
+			.NotEmpty();
+
+		RuleFor(user => user.Name)
+			.NotEmpty();
+
+		RuleFor(user => user.IsStudent)
+			.NotNull();
+
+		RuleFor(user => user.UniversityId)
+			.NotNull();
+	}
+}
