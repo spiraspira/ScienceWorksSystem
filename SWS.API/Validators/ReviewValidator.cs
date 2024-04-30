c6401d70384d201060e841ef418d28624ed5bd25
spiraspira <zabornik5350@gmail.com>
2024-04-30 18:19:39 +0300

    Add ReviewValidator

---
+++ SWS.API/Validators/ReviewValidator.cs
@@ -0,0 +1,16 @@
+﻿namespace SWS.API.Validators;
+
+public class ReviewValidator : AbstractValidator<ReviewViewModel>
+{
+	public ReviewValidator()
+	{
+		RuleFor(nomination => nomination.Text)
+			.NotEmpty();
+
+		RuleFor(nomination => nomination.ReportId)
+			.NotNull();
+
+		RuleFor(nomination => nomination.OrganizationCommitteeMemberId)
+			.NotNull();
+	}
+}
