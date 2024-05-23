ba2eced5c68bc8647f39356eceb8acd22d0b7bfb
spiraspira <zabornik5350@gmail.com>
2024-05-23 23:50:27 +0300

    Add GradeInitial

---
+++ SWS.DAL/InitialData/GradeInitial.cs
@@ -0,0 +1,140 @@
+﻿#pragma warning disable S6562
+namespace SWS.DAL.InitialData;
+
+public static class GradeInitial
+{
+	public static readonly List<Grade> Grades =
+	[
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText1_1_1",
+			ReportGrade = 6,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("734b6c9b-19e9-483d-a540-a6025c8fc1ba"),
+			ProgramCommitteeMemberId = new Guid("9f5618e2-2a9b-45bb-9655-8a6b635e477b"),
+			NominationId = new Guid("48cfadc1-877b-4df3-bd42-a13388bad389"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText1_1_2",
+			ReportGrade = 7,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("734b6c9b-19e9-483d-a540-a6025c8fc1ba"),
+			ProgramCommitteeMemberId = new Guid("54709833-c459-4dbf-b1c5-bbecd4cb8be0"),
+			NominationId = new Guid("48cfadc1-877b-4df3-bd42-a13388bad389"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText1_2_1",
+			ReportGrade = 8,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("734b6c9b-19e9-483d-a540-a6025c8fc1ba"),
+			ProgramCommitteeMemberId = new Guid("9f5618e2-2a9b-45bb-9655-8a6b635e477b"),
+			NominationId = new Guid("d74e442a-de93-4c28-bb51-c47335c48c7b"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText1_2_2",
+			ReportGrade = 9,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("734b6c9b-19e9-483d-a540-a6025c8fc1ba"),
+			ProgramCommitteeMemberId = new Guid("54709833-c459-4dbf-b1c5-bbecd4cb8be0"),
+			NominationId = new Guid("d74e442a-de93-4c28-bb51-c47335c48c7b"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText1_3_1",
+			ReportGrade = 10,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("734b6c9b-19e9-483d-a540-a6025c8fc1ba"),
+			ProgramCommitteeMemberId = new Guid("9f5618e2-2a9b-45bb-9655-8a6b635e477b"),
+			NominationId = new Guid("c99e4ec7-4545-4262-89e2-d83eb570509d"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText1_3_2",
+			ReportGrade = 5,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("734b6c9b-19e9-483d-a540-a6025c8fc1ba"),
+			ProgramCommitteeMemberId = new Guid("54709833-c459-4dbf-b1c5-bbecd4cb8be0"),
+			NominationId = new Guid("c99e4ec7-4545-4262-89e2-d83eb570509d"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText2_1_1",
+			ReportGrade = 6,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("aab2f9fa-cffa-4e34-9039-b4b87911f6dd"),
+			ProgramCommitteeMemberId = new Guid("9f5618e2-2a9b-45bb-9655-8a6b635e477b"),
+			NominationId = new Guid("48cfadc1-877b-4df3-bd42-a13388bad389"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText2_1_2",
+			ReportGrade = 7,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("aab2f9fa-cffa-4e34-9039-b4b87911f6dd"),
+			ProgramCommitteeMemberId = new Guid("54709833-c459-4dbf-b1c5-bbecd4cb8be0"),
+			NominationId = new Guid("48cfadc1-877b-4df3-bd42-a13388bad389"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText2_2_1",
+			ReportGrade = 8,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("aab2f9fa-cffa-4e34-9039-b4b87911f6dd"),
+			ProgramCommitteeMemberId = new Guid("9f5618e2-2a9b-45bb-9655-8a6b635e477b"),
+			NominationId = new Guid("d74e442a-de93-4c28-bb51-c47335c48c7b"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText2_2_2",
+			ReportGrade = 9,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("aab2f9fa-cffa-4e34-9039-b4b87911f6dd"),
+			ProgramCommitteeMemberId = new Guid("54709833-c459-4dbf-b1c5-bbecd4cb8be0"),
+			NominationId = new Guid("d74e442a-de93-4c28-bb51-c47335c48c7b"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText2_3_1",
+			ReportGrade = 10,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("aab2f9fa-cffa-4e34-9039-b4b87911f6dd"),
+			ProgramCommitteeMemberId = new Guid("9f5618e2-2a9b-45bb-9655-8a6b635e477b"),
+			NominationId = new Guid("c99e4ec7-4545-4262-89e2-d83eb570509d"),
+		},
+
+		new Grade
+		{
+			Id = new Guid(""),
+			Text = "GradeText2_3_2",
+			ReportGrade = 6,
+			Date = new DateTime(2023, 2, 10),
+			ReportId = new Guid("aab2f9fa-cffa-4e34-9039-b4b87911f6dd"),
+			ProgramCommitteeMemberId = new Guid("54709833-c459-4dbf-b1c5-bbecd4cb8be0"),
+			NominationId = new Guid("c99e4ec7-4545-4262-89e2-d83eb570509d"),
+		},
+	];
+}
