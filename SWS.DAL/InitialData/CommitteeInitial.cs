e8be44e8c022a8910eff30e99b0e0d39d2b09a72
spiraspira <zabornik5350@gmail.com>
2024-05-23 16:56:17 +0300

    Add CommitteeInitial

---
+++ SWS.DAL/InitialData/CommitteeInitial.cs
@@ -0,0 +1,55 @@
+﻿namespace SWS.DAL.InitialData;
+
+public static class CommitteeInitial
+{
+	public static readonly List<Committee> Committees =
+	[
+		new Committee
+		{
+			Id = new Guid("5e7c8325-15c8-45f2-8393-f9b151d80a52"),
+			TeacherId = new Guid("e27773c6-d514-4d4d-9c62-268a288a2c50")
+		},
+
+		new Committee
+		{
+			Id = new Guid("c40408bb-7935-4b1f-9360-d03ffb6f1fa4"),
+			TeacherId = new Guid("f6d5847c-0901-4e84-86de-4fc20222e4be")
+		},
+
+		new Committee
+		{
+			Id = new Guid("6c234451-67d1-4f9d-9c4c-fd23a2ba86e3"),
+			TeacherId = new Guid("93f21d3b-f696-4663-919e-c7aa20b85e7d")
+		},
+
+		new Committee
+		{
+			Id = new Guid("cf4d6160-bdf1-4f09-a0c0-dcaa25942395"),
+			TeacherId = new Guid("35224b73-1d2e-4065-a529-f1ccb1673427")
+		},
+
+		new Committee
+		{
+			Id = new Guid("5840c4b2-bc1e-438f-975f-2efc63445a89"),
+			TeacherId = new Guid("1aa8aa0e-1222-405c-8b6d-52af5e293d78")
+		},
+
+		new Committee
+		{
+			Id = new Guid("8406469e-2c6a-44fa-8a81-1ab44c34d691"),
+			TeacherId = new Guid("ed6d0164-3a15-43fe-a4e7-71f81e184047")
+		},
+
+		new Committee
+		{
+			Id = new Guid("3226a5eb-b8f9-4c45-b174-808ab13db2de"),
+			TeacherId = new Guid("cc64136a-4bc3-46e5-a194-23968924746b")
+		},
+
+		new Committee
+		{
+			Id = new Guid("51a03ec0-808a-409d-a40a-8a7c20869451"),
+			TeacherId = new Guid("7f1a3891-18c9-4cbb-9790-608ebe7acf29")
+		},
+	];
+}
