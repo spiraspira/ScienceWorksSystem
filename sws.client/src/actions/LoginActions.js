40448809a6612a8e50de0fdb0ab81a5e98d9514b
spiraspira <zabornik5350@gmail.com>
2024-05-24 17:54:14 +0300

    Add token authorization

--- sws.client/src/actions/LoginActions.js
+++ sws.client/src/actions/LoginActions.js
@@ -4,15 +4,18 @@ const LoginActions = {
     login: async (login, password) => {
         try {
             const response = await host.post('api/User/login?login=' + login + '&password=' + password);
-            console.log(response);
-            if (response.data) {
-                return response.data;
-            } else {
-                throw new Error('Неправильный логин или пароль!');
+
+            if (response.data.token) {
+                localStorage.setItem('token', response.data.token);
+                sessionStorage.setItem('token', response.data.token);
+                window.location.href = '/';
             }
         } catch (error) {
-            console.log(error);
-            throw new Error(error.response?.data?.message || error.message || 'Ошибка сервера!');
+            if (error.response?.status === 401) {
+                throw new Error('Неправильный логин или пароль!');
+            } else {
+                throw new Error(error.response?.data?.message || error.message || 'Ошибка сервера!');
+            }
         }
     },
 };
