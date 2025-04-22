1bc8479353dc74e5c3b9b916b8acaae7e580a442
spiraspira <zabornik5350@gmail.com>
2025-04-22 12:13:11 +0300

    Update footer

--- sws.client/src/components/Footer.jsx
+++ sws.client/src/components/Footer.jsx
@@ -4,10 +4,24 @@ import '../App.css';
 
 const Footer = () => {
     return (
-        <AppBar position="static" className="App-header">
-            <Toolbar>
-                <Typography variant="body1" style={{ marginLeft: 'auto' }}>
-                    {new Date().getFullYear()}
+        <AppBar 
+            position="static" 
+            sx={{
+                backgroundColor: '#1976d2',
+                top: 'auto',
+                bottom: 0,
+                marginTop: '40px'
+            }}
+        >
+            <Toolbar sx={{
+                minHeight: '48px',
+                justifyContent: 'center'
+            }}>
+                <Typography variant="body1" sx={{ 
+                    fontSize: '0.875rem',
+                    fontWeight: 500
+                }}>
+                    © {new Date().getFullYear()} Все права защищены
                 </Typography>
             </Toolbar>
         </AppBar>
